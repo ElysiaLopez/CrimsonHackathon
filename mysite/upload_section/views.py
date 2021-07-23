2c85284dfe1bc38296544b4e752d3e3829ebb88d
Elysia Lopez <elysialopez528@gmail.com>
2021-07-23 11:43:06 -0700

    Started working on grabbing all posts from the databse and displaying them on the upload_section/index page. Created index.html file

--- mysite/upload_section/views.py
+++ mysite/upload_section/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import UserPost, TextPost, ImagePost, FilePost
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('This is the upload page')
+    posts_list = UserPost.objects.order_by('-pub_date')
+    context = {'posts_list': posts_list}
+    return render(request, 'upload_section/index.html', context)
+
+def displayPosts():
+    context = []
+    
